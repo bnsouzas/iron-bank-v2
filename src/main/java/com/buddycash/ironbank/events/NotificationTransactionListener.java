a850995313b9d9469093df6c51f963d1dfc4ce73
Bruno Souza <bnsouzas@gmail.com>
2025-08-24 07:16:20 -0300

    disable mongo

--- src/main/java/com/buddycash/ironbank/events/NotificationTransactionListener.java
+++ src/main/java/com/buddycash/ironbank/events/NotificationTransactionListener.java
@@ -11,29 +11,29 @@ import org.springframework.stereotype.Service;
 @Service
 public class NotificationTransactionListener
     implements MessageListener<String, TransactionDataEvent> {
-  private final TransactionMongoRepository transactionMongoRepository;
+  // private final TransactionMongoRepository transactionMongoRepository;
 
-  public NotificationTransactionListener(TransactionMongoRepository transactionMongoRepository) {
-    this.transactionMongoRepository = transactionMongoRepository;
-  }
+  // public NotificationTransactionListener(TransactionMongoRepository transactionMongoRepository) {
+  //   this.transactionMongoRepository = transactionMongoRepository;
+  // }
 
   @Override
   @KafkaListener(topics = "${app.topics.transaction}", groupId = "${spring.kafka.consumer.group-id}")
   public void onMessage(ConsumerRecord<String, TransactionDataEvent> data) {
     var event = data.value();
-    var transaction = event.data();
-    if (!data.value().type().equals(DataEventType.DELETE)) {
-      transactionMongoRepository.save(data.value().data());
-    } else {
-      transactionMongoRepository.deleteById(data.value().data().id());
-    }
+    // var transaction = event.data();
+    // if (!data.value().type().equals(DataEventType.DELETE)) {
+    //   transactionMongoRepository.save(data.value().data());
+    // } else {
+    //   transactionMongoRepository.deleteById(data.value().data().id());
+    // }
     System.out.println(
         String.format(
             "%s %s %s %s with price %7.2f",
             event.eventTime(),
             event.type(),
-            transaction.type(),
-            transaction.name(),
-            transaction.price()));
+            event.data().type(),
+            event.data().name(),
+            event.data().price()));
   }
 }
